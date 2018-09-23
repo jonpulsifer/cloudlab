29f408a11ba7207786426e188fcc4ac1037c0aa8
Jonathan Pulsifer <jonathan.pulsifer@shopify.com>
2018-09-23 19:16:15 +0000

    yolo module
    Signed-off-by: Jonathan Pulsifer <jonathan.pulsifer@shopify.com>

---
+++ terraform/modules/gce-cos-vm/variables.tf
@@ -0,0 +1,31 @@
+variable "name" {
+  description = "Name for the service account and VM prefix"
+  default     = "lab"
+}
+
+variable "image_config" {
+  type = "map"
+
+  default = {
+    family  = "ubuntu-1804-lts"
+    project = "trusted-builds"
+  }
+}
+
+variable "instance_config" {
+  type = "map"
+
+  default = {
+    online       = false
+    machine_type = "n1-standard-1"
+    preemptible  = "false"
+    subnet       = "10.13.37.0/29"
+    linux_user   = "alice"
+    mount_point  = "/mnt/disk2"
+    ssd_size     = 10
+
+    metadata_startup_script = <<HEREDOC
+#cloud-config
+HEREDOC
+  }
+}
