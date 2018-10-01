50dcacbc930766bf243e6ca097130551dc92ca85
Jonathan Pulsifer <jonathan@pulsifer.ca>
2018-09-30 21:20:37 -0400

    move service files away from cloud-init
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- projects/lab/bullseye.tf
+++ projects/lab/bullseye.tf
@@ -25,55 +25,10 @@ module "cos-vm" {
 
   instance_config = {
     online       = "${local.online}"
+    cloud_init   = "${file("../../cloud-init/bullseye")}"
     machine_type = "n1-standard-1"
+    ssd_size     = "10"
     subnet       = "${module.network.subnet}"
-    ssd_size     = "${local.ssd_size}"
     preemptible  = false
-
-    cloud_init = <<HEREDOC
-#cloud-config
-
-write_files:
-- path: /etc/systemd/system/bullseye.service
-  permissions: 0644
-  owner: root
-  content: |
-    [Unit]
-    Description=Start the bullseye container
-    Wants=gcr-online.target
-    After=gcr-online.target
-
-    [Service]
-    Environment="HOME=/home/cloudservice"
-    ExecStartPre=/usr/bin/docker-credential-gcr configure-docker
-    ExecStartPre=/sbin/iptables -w -A INPUT -p tcp --dport 30022 -j ACCEPT
-    ExecStart=/usr/bin/docker run --rm --name=bullseye --network=host -v /home/bullseye:/home/jawn gcr.io/trusted-builds/cloudlab-linux-build:latest
-    ExecStop=/usr/bin/docker stop bullseye
-    ExecStopPost=/usr/bin/docker rm bullseye
-
-- path: /etc/systemd/system/datadog.service
-  permissions: 0644
-  owner: root
-  content: |
-    [Unit]
-    Description=Datadog Agent container
-
-    [Service]
-    Environment="HOME=/home/cloudservice"
-    ExecStart=/usr/bin/docker run --rm --name=datadog -v /var/run/docker.sock:/var/run/docker.sock:ro -v /proc/:/host/proc/:ro -v /sys/fs/cgroup/:/host/sys/fs/cgroup:ro -v /etc/passwd:/etc/passwd:ro -e DD_PROCESS_AGENT_ENABLED=true -e DD_API_KEY=lolol datadog/agent:latest
-    ExecStop=/usr/bin/docker stop datadog
-    ExecStopPost=/usr/bin/docker rm datadog
-
-runcmd:
-- systemctl daemon-reload
-- systemctl start bullseye.service
-- systemctl start datadog.service
-
-# Optional once-per-boot setup. Ex: mounting a PD.
-bootcmd:
-- mkfs.ext4 -m 0 -F -E lazy_itable_init=0,lazy_journal_init=0,discard /dev/sdb
-- mkdir -p /home/bullseye
-- mount -o discard,defaults /dev/sdb /home/bullseye
-HEREDOC
   }
 }
