c7f6b9db4219dcae5bee1db4abcb29c8ad90f137
Jonathan Pulsifer <jonathan.pulsifer@shopify.com>
2019-01-09 22:40:16 -0500

    add containerd nodepool
    Signed-off-by: Jonathan Pulsifer <jonathan.pulsifer@shopify.com>

--- gcp/projects/kubesec/bullseye.tf
+++ gcp/projects/kubesec/bullseye.tf
@@ -39,14 +39,34 @@ module "gke" {
   cluster_config = {
     online               = true
     node_count           = 1
+    image_type           = "COS"
     machine_type         = "n1-standard-1"
     disk_size_gb         = 20
     preemptible          = false
     metadata_proxy       = false
     network_policy       = false
+    istio                = true
     binary_authorization = true
     node_cidr            = "10.13.37.0/24"
     service_cidr         = "10.20.30.0/24"
     pod_cidr             = "10.100.0.0/19"
+  }
+}
+
+module "nodepool" {
+  source = "../../../terraform/modules/gke-beta-nodepool"
+
+  nodepool_config = {
+    online             = true
+    name               = "small"
+    cluster            = "cloudlab"
+    node_count         = 2
+    image_type         = "COS_CONTAINERD"
+    kubernetes_version = "1.11.6-gke.0"
+    service_account    = "${module.gke.node_service_account}"
+    machine_type       = "g1-small"
+    disk_size_gb       = 10
+    preemptible        = true
+    metadata_proxy     = false
   }
 }
