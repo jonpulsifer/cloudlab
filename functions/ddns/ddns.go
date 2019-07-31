418e18a84903e318ba94a9646392d5d50fe7f2ec
Jonathan Pulsifer <jonathan.pulsifer@shopify.com>
2019-07-31 13:53:30 -0400

    moar better
    Signed-off-by: Jonathan Pulsifer <jonathan.pulsifer@shopify.com>

--- functions/ddns/ddns.go
+++ functions/ddns/ddns.go
@@ -2,26 +2,30 @@ package ddns
 
 import (
 	"encoding/json"
+	"errors"
+	"net/http"
+	"os"
+	"strings"
+
 	log "github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
 	"golang.org/x/oauth2/google"
 	dns "google.golang.org/api/dns/v1"
-	"net/http"
-	"os"
-	"strings"
 )
 
 func init() {
 	log.SetLevel(log.InfoLevel)
 }
 
-type DdnsRequest struct {
-	IpAddress string `json:"ipaddress"`
-	DnsName   string `json:"dnsname"`
-	ApiToken  string `json:"apitoken"`
+// Request is the expected struct we want as a request
+type Request struct {
+	IPAddress string `json:"IPAddress"`
+	DNSName   string `json:"DNSName"`
+	APIToken  string `json:"APIToken"`
 }
 
-type DdnsResponse struct {
+// Response is the response we expect to return
+type Response struct {
 	Status    string `json:"status"`
 	Additions int    `json:"additions"`
 	Deletions int    `json:"deletions,omitempty"`
@@ -29,43 +33,45 @@ type DdnsResponse struct {
 
 // UpdateDDNS is an HTTP Cloud Function with a request parameter.
 func UpdateDDNS(w http.ResponseWriter, r *http.Request) {
-	var apiToken string = os.Getenv("API_TOKEN")
-	var project string = os.Getenv("GCP_PROJECT")
-	var ddnsRequest DdnsRequest
+	var apiToken = os.Getenv("API_TOKEN")
+	var project = os.Getenv("GCP_PROJECT")
+	var request Request
 
 	if project == "" {
 		project = "homelab-ng"
 	}
 
-	if err := json.NewDecoder(r.Body).Decode(&ddnsRequest); err != nil {
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		log.Errorf("Could not decode request JSON: %v", err.Error())
+		return
 	}
 
-	if ddnsRequest.ApiToken != apiToken {
+	if request.APIToken != apiToken {
 		http.Error(w, http.StatusText(http.StatusUnauthorized), http.StatusUnauthorized)
-		log.Errorf("Invalid API token received: %s", ddnsRequest.ApiToken)
+		log.Errorf("Invalid API token received: %s", request.APIToken)
+		return
 	}
 
-	client, err := getCloudDnsService()
+	client, err := getCloudDNSService()
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		log.Fatalf("Could not get DNS service: %v", err.Error())
 	}
 
-	managedZone, err := getManagedZoneFromDnsName(client, project, ddnsRequest.DnsName)
+	managedZone, err := getManagedZoneFromDNSName(client, project, request.DNSName)
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		log.Fatalf("Could not get Managed Zone: %v", err.Error())
 	}
 
-	change, err := getDnsChange(client, project, managedZone, &ddnsRequest)
+	change, err := getDNSChange(client, project, managedZone, &request)
 	if err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
 		log.Fatalf("Could not create DNS change: %v", err.Error())
 	}
 
-	response := DdnsResponse{
+	response := Response{
 		Status:    change.Status,
 		Additions: len(change.Additions),
 		Deletions: len(change.Deletions),
@@ -73,19 +79,19 @@ func UpdateDDNS(w http.ResponseWriter, r *http.Request) {
 
 	if err := json.NewEncoder(w).Encode(response); err != nil {
 		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
-		log.Errorf("Could not encode response: %v", err.Error())
+		log.Fatalf("Could not encode response: %v", err.Error())
 	}
 	log.WithFields(log.Fields{
 		"project":     project,
 		"managedZone": managedZone.Name,
-		"dnsname":     ddnsRequest.DnsName,
-		"ip":          ddnsRequest.IpAddress,
+		"dnsame":      request.DNSName,
+		"ip":          request.IPAddress,
 		"status":      change.Status,
 		"statuscode":  change.HTTPStatusCode,
 	}).Infof("DNS Change Requested")
 }
 
-func getCloudDnsService() (*dns.Service, error) {
+func getCloudDNSService() (*dns.Service, error) {
 	c, err := google.DefaultClient(context.Background(), dns.CloudPlatformScope)
 	if err != nil {
 		return nil, err
@@ -93,16 +99,16 @@ func getCloudDnsService() (*dns.Service, error) {
 	return dns.New(c)
 }
 
-func getDnsChange(client *dns.Service, project string, managedZone *dns.ManagedZone, ddns *DdnsRequest) (*dns.Change, error) {
+func getDNSChange(client *dns.Service, project string, managedZone *dns.ManagedZone, request *Request) (*dns.Change, error) {
 	var (
 		additions  []*dns.ResourceRecordSet
 		deletions  []*dns.ResourceRecordSet
 		change     dns.Change
 		dnsRequest = &dns.ResourceRecordSet{
-			Name:    ddns.DnsName,
+			Name:    request.DNSName,
 			Type:    "A",
 			Ttl:     60,
-			Rrdatas: []string{ddns.IpAddress},
+			Rrdatas: []string{request.IPAddress},
 		}
 	)
 
@@ -113,7 +119,7 @@ func getDnsChange(client *dns.Service, project string, managedZone *dns.ManagedZ
 	}
 
 	for _, recordset := range resp.Rrsets {
-		if recordset.Name == ddns.DnsName {
+		if recordset.Name == request.DNSName && recordset.Type == "A" {
 			deletions = append(deletions, recordset)
 		}
 	}
@@ -122,8 +128,8 @@ func getDnsChange(client *dns.Service, project string, managedZone *dns.ManagedZ
 	log.WithFields(log.Fields{
 		"additions": len(additions),
 		"deletions": len(deletions),
-		"endpoint":  ddns.DnsName,
-		"ip":        ddns.IpAddress,
+		"endpoint":  request.DNSName,
+		"ip":        request.IPAddress,
 	}).Debugf("Preparing DNS change")
 
 	if len(deletions) == 0 {
@@ -140,16 +146,16 @@ func getDnsChange(client *dns.Service, project string, managedZone *dns.ManagedZ
 	return client.Changes.Create(project, managedZone.Name, &change).Context(context.Background()).Do()
 }
 
-func getManagedZoneFromDnsName(c *dns.Service, project string, dnsName string) (*dns.ManagedZone, error) {
+func getManagedZoneFromDNSName(c *dns.Service, project string, DNSName string) (*dns.ManagedZone, error) {
 	managedZoneList, err := c.ManagedZones.List(project).Do()
 	if err != nil {
 		return nil, err
 	}
 
 	for _, managedZone := range managedZoneList.ManagedZones {
-		if strings.HasSuffix(dnsName, managedZone.DnsName) {
+		if strings.HasSuffix(DNSName, managedZone.DnsName) {
 			return managedZone, nil
 		}
 	}
-	return nil, nil
+	return nil, errors.New("No managed zone found")
 }
--- functions/ddns/ddns_test.go
+++ functions/ddns/ddns_test.go
@@ -1,44 +1,97 @@
 package ddns
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http/httptest"
-	"strings"
+	"reflect"
 	"testing"
+
+	dns "google.golang.org/api/dns/v1"
 )
 
-func TestUpdateDDNS(t *testing.T) {
-	req1 := DdnsRequest{IpAddress: "127.0.0.1", DnsName: "test1.home.pulsifer.ca.", ApiToken: ""}
-	req2 := DdnsRequest{IpAddress: "127.0.0.2", DnsName: "test2.home.pulsifer.ca.", ApiToken: ""}
-	req3 := DdnsRequest{IpAddress: "127.0.0.3", DnsName: "test3.home.pulsifer.ca."}
-	req4 := DdnsRequest{IpAddress: "127.0.0.4", DnsName: "test5.home.pulsifer.ca."}
-
+func Test_UpdateDDNS(t *testing.T) {
 	tests := []struct {
-		request DdnsRequest
-		want    DdnsResponse
+		request Request
+		want    Response
 	}{
-		{request: req1, want: DdnsResponse{Status: "pending", Additions: 1, Deletions: 1}},
-		{request: req2, want: DdnsResponse{Status: "pending", Additions: 1, Deletions: 1}},
-		{request: req3, want: DdnsResponse{Status: "pending", Additions: 1, Deletions: 1}},
-		{request: req4, want: DdnsResponse{Status: "pending", Additions: 1, Deletions: 1}},
+		{request: Request{IPAddress: "127.0.0.1", DNSName: "test1.home.pulsifer.ca.", APIToken: ""}, want: Response{Status: "pending", Additions: 1, Deletions: 1}},
+		{request: Request{IPAddress: "127.0.0.2", DNSName: "home.pulsifer.ca.", APIToken: ""}, want: Response{Status: "pending", Additions: 1, Deletions: 1}},
+		{request: Request{IPAddress: "127.0.0.3", DNSName: "test3.home.pulsifer.ca."}, want: Response{Status: "pending", Additions: 1, Deletions: 1}},
+		{request: Request{IPAddress: "127.0.0.4", DNSName: "test5.home.pulsifer.ca."}, want: Response{Status: "pending", Additions: 1, Deletions: 1}},
 	}
 
 	for _, test := range tests {
-		var out DdnsResponse
+		var got Response
 		ddnsReq, _ := json.Marshal(test.request)
-		ddnsResp, _ := json.Marshal(test.want)
-
-		req := httptest.NewRequest("GET", "/ddns", strings.NewReader(string(ddnsReq)))
+		req := httptest.NewRequest("GET", "/ddns", bytes.NewReader(ddnsReq))
 		req.Header.Add("Content-Type", "application/json")
 
 		rr := httptest.NewRecorder()
 		UpdateDDNS(rr, req)
 
-		if err := json.NewDecoder(rr.Body).Decode(&out); err != nil {
+		if err := json.NewDecoder(rr.Body).Decode(&got); err != nil {
 			t.Fatalf("Could not decode response body JSON: %v", err)
 		}
-		if got := out; got != test.want {
-			t.Errorf("UpdateDDNS(%s) = %v, want %s", ddnsReq, got, ddnsResp)
+		if !reflect.DeepEqual(got, test.want) {
+			t.Errorf("UpdateDDNS() = %v, want %v", got, test.want)
 		}
+	}
+}
+
+func Test_getDNSChange(t *testing.T) {
+	type args struct {
+		client      *dns.Service
+		project     string
+		managedZone *dns.ManagedZone
+		request     *Request
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *dns.Change
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getDNSChange(tt.args.client, tt.args.project, tt.args.managedZone, tt.args.request)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getDNSChange() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getDNSChange() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_getManagedZoneFromDNSName(t *testing.T) {
+	type args struct {
+		c       *dns.Service
+		project string
+		DNSName string
+	}
+	tests := []struct {
+		name    string
+		args    args
+		want    *dns.ManagedZone
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got, err := getManagedZoneFromDNSName(tt.args.c, tt.args.project, tt.args.DNSName)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("getManagedZoneFromDNSName() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("getManagedZoneFromDNSName() = %v, want %v", got, tt.want)
+			}
+		})
 	}
 }
