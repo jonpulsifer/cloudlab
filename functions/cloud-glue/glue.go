038327f9a1ff920778aa3a86a95c7a753d95d738
Jonathan Pulsifer <jonathan@pulsifer.ca>
2020-04-26 05:00:15 +0000

    ok ok ok func
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

---
+++ functions/cloud-glue/glue.go
@@ -0,0 +1,38 @@
+package cloudfunction
+
+import (
+	"encoding/json"
+	"net/http"
+
+	log "github.com/sirupsen/logrus"
+)
+
+type GasRequest struct {
+	Email       string `json:"email"`
+	Spreadsheet string `json:"spreadsheet,omitempty"`
+	Sheet       string `json:"sheet,omitempty"`
+	Score       int    `json:"score,omittempty"`
+}
+
+type GasResponse struct {
+	Status string `json:"status"`
+}
+
+func init() { log.SetLevel(log.InfoLevel) }
+func AppScriptHandler(w http.ResponseWriter, r *http.Request) {
+	var request GasRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		log.Errorf("Could not decode request from Google Apps Script: %v", err.Error())
+		return
+	}
+
+	response := GasResponse{
+		Status: "ok",
+	}
+
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		log.Errorf("Could not encode response: %v", err.Error())
+	}
+}
