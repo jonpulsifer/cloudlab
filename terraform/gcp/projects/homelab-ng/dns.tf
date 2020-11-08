6a82d3ea1ab2e7ca0c6282698b8e7a9689425b43
Jonathan Pulsifer <jonathan@pulsifer.ca>
2020-11-08 22:27:22 +0000

    bump
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- terraform/gcp/projects/homelab-ng/dns.tf
+++ terraform/gcp/projects/homelab-ng/dns.tf
@@ -8,3 +8,13 @@ resource "google_dns_managed_zone" "home-pulsifer-ca" {
     environment = "home"
   }
 }
+resource "google_dns_managed_zone" "wishlist-app" {
+  name        = "wishlist-app"
+  dns_name    = "wishin.app."
+  description = "Domain for the wishlist app"
+
+  labels = {
+    domain      = "wishlist-app"
+    environment = "production"
+  }
+}
--- terraform/gcp/projects/wishlist-4362e/services.tf
+++
@@ -1,4 +0,0 @@
-resource "google_project_service" "clouddns" {
-  service = "dns.googleapis.com"
-  disable_dependent_services = true
-}
